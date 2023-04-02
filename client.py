e37f076c383a763fd085ba1093b9cb705f37f858
zhangta6 <zhangta6@bu.edu>
2023-04-02 15:59:59 -0400

    ver2.0 draft

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP, SERVER_PORT))
+
+def receive():
+    while True:
+        try:
+            msg = client.recv(1024).decode('utf-8')
+            print(msg)
+        except Exception as e:
+            print(f"[ERROR] {e}")
+            break
+
+def send(msg):
+    client.send(msg.encode('utf-8'))
+
+client_name = input("Enter your name: ")
+send(client_name)
+
+thread = threading.Thread(target=receive)
+thread.start()
+
+while True:
+    msg = input()
+    if msg == "DISCONNECT":
+        send(msg)
+        break
+    elif msg == "QUERY":
+        send(msg)
+    else:
+        send(msg)
---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((SERVER_IP, SERVER_PORT))
+server.listen()
+
+clients = {}
+
+def handle_client(client, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    client_name = client.recv(1024).decode('utf-8')
+    clients[client_name] = addr
+
+    while True:
+        try:
+            msg = client.recv(1024).decode('utf-8')
+            if msg == "DISCONNECT":
+                del clients[client_name]
+                client.close()
+                break
+            elif msg == "QUERY":
+                client.send(str(clients).encode('utf-8'))
+            else:
+                recipient, message = msg.split(":", 1)
+                recipient_addr = clients.get(recipient)
+                if recipient_addr:
+                    client.send(f"{client_name}: {message}".encode('utf-8'))
+                else:
+                    client.send("Recipient not found.".encode('utf-8'))
+        except Exception as e:
+            print(f"[ERROR] {e}")
+            break
+
+    print(f"[DISCONNECTED] {addr} disconnected.")
+
+while True:
+    client, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(client, addr))
+    thread.start()
